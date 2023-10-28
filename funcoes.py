273e538d4aec4d4f99ef70bd1eb44c1be473a74a
Arthur Brito <arthurbg951@hotmail.com>
2023-10-28 15:45:34 -0300

    Adicionando docstrings

--- funcoes.py
+++ funcoes.py
@@ -1,12 +1,23 @@
 import math
 
 def arredonda_pra_cima(valor: float) -> int:
+    '''
+    Arredonda um valor para cima. Ex: 7.15 -> 8
+    '''
     return int(valor + (1 - valor % 1))
 
 def arredonda_pra_baixo(valor: float) -> int:
+    '''
+    Arredonda um valor para baixo. Ex: 7.15 -> 7
+    '''
     return int(valor - (valor % 1))
 
 def check_bitola(area_aco: float, *args: tuple[int, float]) -> None:
+    '''
+    Verifica multiplas bitolas para um mesmo As. 
+    *args pode conter várias tuplas no formato:
+    (num_bitolas1, bitola1), (num_bitolas2, bitola2) ...
+    '''
     aco_total = 0
     qtd_total_bitolas = 0
     for qtd_bitolas, bitola in args:
